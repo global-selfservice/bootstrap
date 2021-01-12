eb0feabf7e9c42bc562e0ab8df34b9792fc03d13
Vinod Sai Kumar <60657267+vinod-global@users.noreply.github.com>
2021-01-12 15:09:30 +0000

    fix: upgraded to latest modules (#3)

--- common.tf
+++
@@ -1,3 +0,0 @@
-module "common" {
-  source = "github.com/global-devops-terraform/global-info?ref=v0.34.1"
-}
--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "terraform" {
-  source = "github.com/global-devops-terraform/terraform?ref=v1.0.0"
+  source = "github.com/global-devops-terraform/terraform?ref=v1.2.0"
 
   bucket_name = var.bucket_name
 }
 
 module "jenkins" {
-  source = "github.com/global-devops-terraform/jenkins-roles?ref=v1.1.0"
+  source = "github.com/global-devops-terraform/jenkins-roles?ref=v1.4.0"
 
-  bucket_name  = module.terraform.bucket_name
-  jenkins_role = module.common.jenkins_oidc_roles["selfservice"]
+  bucket_name      = module.terraform.bucket_name
+  jenkins_instance = "selfservice"
 }
